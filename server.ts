0a39c92f2b60ae44f8bb7b9823b1fa2ffb42c704
Zhongyi Chen <zhongyi753@gmail.com>
2020-10-10 17:57:13 -0500

    setup sandbox files

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import * as socketio from 'socket.io';
 import * as serveStatic from 'serve-static';
 import * as path from 'path';
 import * as util from 'util';
+import { Temp } from './temp';
 
 
 const app = express();
@@ -22,12 +23,15 @@ httpServer.listen(3000, () => {
     console.log(`Server ready`);
 });
 
+var temp = new Temp();
+
 
 io.on('connection', (socket) => {
     console.log('got connection');
     socket.on('test-send-1', (data) => {
         console.log(data);
     });
+    socket.emit('init-connection', temp.data);
 });
 
 
---
+++ temp.ts
@@ -0,0 +1,18 @@
+
+export interface Cube {
+    x: number,
+    y: number,
+    z: number
+}
+
+export class Temp {
+    public data: Cube[];
+
+    constructor() {
+        this.data = [];
+    }
+
+    private foo(): void {
+
+    }
+}
